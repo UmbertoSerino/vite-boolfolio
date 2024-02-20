40c58ef8c8c7584d5f4d9c7e9b7db31b964180d5
UmbertoSerino <serumberto@gmail.com>
2024-02-20 19:22:07 +0100

    Added card with v-for projects

--- vite-boolfolio/src/components/AppMain.vue
+++ vite-boolfolio/src/components/AppMain.vue
@@ -4,7 +4,7 @@ export default {
     name: 'AppMain',
     data() {
         return {
-
+            projects: [],
         }
     },
     methods: {
@@ -34,8 +34,27 @@ export default {
         <h1 class="text-center">
             Main
         </h1>
+        <div class="row">
+            <div class="col-4" v-for="project in projects" :key="project.id">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Titolo: {{ project.title }}</h5>
+                        <p class="card-text"><strong>Descrizione: </strong>{{ project.description }}</p>
+                        <p class="card-text"><strong>Data: </strong>{{ project.date }}</p>
+                        <p class="card-text"><strong>Autore: </strong>{{ project.user.name }}</p>
+                        <p class="card-text"><strong>Tipo di Tecnologia: </strong>{{ project.type.name }}</p>
+                        <p class="card-text"><strong>Tecnologie: </strong></p>
+                        <ul>
+                            <li v-for="technology in project.technologies" :key="technology.id">{{ technology.name }}</li>
+                        </ul>
+                        <a :href="project.url" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
+
 <style lang="scss" scoped>
 @use '../style/partials/mixins' as*;
 @use '../style/partials/variables' as*;
